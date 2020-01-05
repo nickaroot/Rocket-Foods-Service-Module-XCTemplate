75f31bce9bbe8262aa694b80eb8cf24bbe7ef9af
nickaroot <funtrum@icloud.com>
2020-01-05 06:05:39 +0300

    Optional Promise removed

--- ___VARIABLE_modulePrefix___Service+___VARIABLE_entitiesCapitalized___.swift
+++ ___VARIABLE_modulePrefix___Service+___VARIABLE_entitiesCapitalized___.swift
@@ -4,36 +4,36 @@ import PromiseKit
 import RealmSwift
 
 extension ___VARIABLE_modulePrefix___Service {
-    public func all___VARIABLE_entitiesCapitalized___() -> Promise<[___VARIABLE_entityCapitalized___Model]?>? {
-        DispatchQueue.global(qos: .userInitiated).async(.promise) { [weak self] in
-            guard let realmConfig = self?.realmConfig else { return nil }
+    public func all___VARIABLE_entitiesCapitalized___() -> Promise<[___VARIABLE_entityCapitalized___Model]> {
+        Promise { [weak self] seal in
+            guard let realmConfig = self?.realmConfig else { return seal.reject(PMKError.cancelled) }
             
             let realm = try? Realm(configuration: realmConfig)
             
-            guard let objects = realm?.objects(___VARIABLE_entityCapitalized___Object.self).sorted(byKeyPath: "id") else { return nil }
+            guard let objects = realm?.objects(___VARIABLE_entityCapitalized___Object.self).sorted(byKeyPath: "id") else { return seal.reject(PMKError.cancelled) }
             
             let ___VARIABLE_entitiesLowercased___ = Array(objects).compactMap { ___VARIABLE_entityCapitalized___Model($0) }
             
-            return ___VARIABLE_entitiesLowercased___
+            return seal.fulfill(___VARIABLE_entitiesLowercased___)
         }
     }
     
-    public func ___VARIABLE_entitiesLowercased___Count() -> Int? {
+    public func ___VARIABLE_entitiesLowercased___Count() -> Int {
         let realm = try? Realm(configuration: realmConfig)
         
         return realm?.objects(___VARIABLE_entityCapitalized___Object.self).count ?? 0
     }
     
-    public func ___VARIABLE_entityLowercased___(modelAtIndex index: Int) -> Promise<___VARIABLE_entityCapitalized___Model?>? {
-        DispatchQueue.global(qos: .userInitiated).async(.promise) { [weak self] in
-            guard let realmConfig = self?.realmConfig else { return nil }
+    public func ___VARIABLE_entityLowercased___(modelAtIndex index: Int) -> Promise<___VARIABLE_entityCapitalized___Model> {
+        Promise { [weak self] seal in
+            guard let realmConfig = self?.realmConfig else { return seal.reject(PMKError.cancelled) }
             
             let realm = try? Realm(configuration: realmConfig)
             
             guard let count = realm?.objects(___VARIABLE_entityCapitalized___Object.self).count, index < count,
-                let object = realm?.objects(___VARIABLE_entityCapitalized___Object.self)[index] else { return nil }
+                let object = realm?.objects(___VARIABLE_entityCapitalized___Object.self)[index] else { return seal.reject(PMKError.cancelled) }
             
-            return ___VARIABLE_entityCapitalized___Model(object)
+            return seal.fulfill(___VARIABLE_entityCapitalized___Model(object))
         }
     }
 }
