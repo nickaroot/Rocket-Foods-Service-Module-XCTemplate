2a095d3dfae388c89f14866661d0761b7a059628
nickaroot <funtrum@icloud.com>
2021-05-08 09:20:15 +0300

    Object -> ManagedObject fix

--- ___VARIABLE_modulePrefix___Service+___VARIABLE_entitiesCapitalized___.swift
+++ ___VARIABLE_modulePrefix___Service+___VARIABLE_entitiesCapitalized___.swift
@@ -6,7 +6,7 @@ extension ___VARIABLE_modulePrefix___Service {
     public func all___VARIABLE_entitiesCapitalized___() -> [Model] {
         guard let realm = try? Realm(configuration: realmConfig) else { return [] }
         
-        let objects = realm.objects(Model.Object.self).sortedByPersistOrder
+        let objects = realm.objects(Model.ManagedObject.self).sortedByPersistOrder
         
         let ___VARIABLE_entitiesLowercased___ = Array(objects).compactMap { Model($0) }
         
@@ -16,15 +16,15 @@ extension ___VARIABLE_modulePrefix___Service {
     public func ___VARIABLE_entitiesLowercased___Count() -> Int {
         guard let realm = try? Realm(configuration: realmConfig) else { return 0 }
         
-        return realm.objects(Model.Object.self).count
+        return realm.objects(Model.ManagedObject.self).count
     }
     
     public func ___VARIABLE_entityLowercased___(modelAtIndex index: Int) -> Model? {
         guard let realm = try? Realm(configuration: realmConfig) else { return nil }
         
-        guard index < realm.objects(Model.Object.self).count else { return nil }
+        guard index < realm.objects(Model.ManagedObject.self).count else { return nil }
         
-        let object = realm.objects(Model.Object.self).sortedByPersistOrder[index]
+        let object = realm.objects(Model.ManagedObject.self).sortedByPersistOrder[index]
         
         let model = Model(object)
         
